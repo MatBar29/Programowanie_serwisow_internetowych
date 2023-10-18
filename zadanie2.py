98d769fc922a4f0269525a82591caaf0a6f95af5
MatBar29 <92025651+MatBar29@users.noreply.github.com>
2023-10-18 15:24:00 +0200

    zadanie2.py

--- zadanie2.py
+++ zadanie2.py
@@ -1,12 +1,30 @@
 import requests
-import json
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 
-url = 'https://www.meteoprog.pl/pl/weather/Olsztyn/'
+url = "https://www.meteoprog.pl/pl/weather/Olsztyn/"
+
 response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-soup = BeautifulSoup(response.content, features="html.parser")
-table = soup.find('ul', class_='today-hourly-weather hide-scroll').find('li')
+temp_now = soup.find('span', class_='today-temperature')
 
-print(table)
+hourly_temp_section = soup.find('ul', class_='today-hourly-weather')
+
+hours = []
+temperatures = []
+
+for hour_block in hourly_temp_section.find_all('li'):
+    hour = hour_block.find('span', class_='today-hourly-weather__name').text
+    temperature = hour_block.find('span', class_='today-hourly-weather__temp').text
+    temperatures.append(int(temperature.replace("°", "")))
+    hours.append(hour)
+
+plt.figure(figsize=(10, 5))
+plt.plot(hours, temperatures, marker='o')
+plt.title(f'Temperatura w Olsztynie na najbliższe godziny')
+plt.xlabel('Godzina')
+plt.ylabel('Temperatura (°C)')
+plt.grid(True)
+
+plt.show()
